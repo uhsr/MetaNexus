eeb9f9bb31fa18b0e498b78193ef2fb9c7b1425a
uhsr <uhsr@eteb.me>
2025-08-02 20:55:49 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/metanexus/main.go
@@ -0,0 +1,23 @@
+// cmd/metanexus/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"metanexus/internal/metanexus"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := metanexus.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
